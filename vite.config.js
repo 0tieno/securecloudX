ef125af7c75c02d86fa3cb28009f878d7b5053f9
0tieno <ronvi035@gmail.com>
2025-03-21 04:35:27 +0300

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/SecureCloudX/', // ✅ Correct base path for GitHub Pages
   plugins: [
+    react(),
     tailwindcss(),
-    react()
   ],
 })
