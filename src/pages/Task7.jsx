02b5691b3322206b4c6ef3d3429bedb18338b51a
0tieno <ronvi035@gmail.com>
2025-03-26 18:42:18 +0300

    fix: update report submission link in Task7 component

--- src/pages/Task7.jsx
+++ src/pages/Task7.jsx
@@ -133,7 +133,7 @@ I want this to feel like a real-world security project where I’m learning by d
     Complete your **detailed report** and submit it to **receive your official Certificate** from Microsoft Learn.  
   </p>
   <a 
-    href="https://forms.microsoft.com/" 
+    href="https://forms.office.com/r/imQADHyNn7" 
     className="inline-block mt-3 px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md transition duration-200"
   >
     📝 Submit Report
