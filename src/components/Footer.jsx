8b92cf66fa1a50b548edcdeb2a3dcfe01e2d0e39
0tieno <ronvi035@gmail.com>
2025-04-22 17:48:16 +0300

    renamed titles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer className="bg-gray-900 text-gray-400 text-center p-4 mt-8">
-      <p className="text-sm">&copy; {new Date().getFullYear()} secureCloudX. All rights reserved.</p>
+      <p className="text-sm">&copy; {new Date().getFullYear()} securecloudX. All rights reserved.</p>
       <a 
         href="mailto:securecloudx.learn@gmail.com" 
         className="text-blue-400 text-xs mt-1 hover:underline"
