ba790e09694b7a3dd19cc4f8503ad2ee97a18fdf
0tieno <ronvi035@gmail.com>
2025-08-26 20:32:10 +0300

    feat: Refactor header section in Home component for improved layout and clarity

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,20 +55,25 @@ const Home = () => {
             </div>
           </Link>
         </div>
-        <h1 className="text-2xl md:text-4xl font-bold mb-6 mt-4 text-start">
-          Welcome
-        </h1>
-
-       <div className="flex items-center  text-gray-400 text-sm mb-4 border-l-2 border-blue-500 pl-3 py-1">
-              <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span>Last Updated Aug 25, 2025</span>
-            </div>
+        <div className="flex items-center mb-6 mt-4">
+          <h1 className="text-2xl md:text-4xl font-bold text-start mr-4">
+            Welcome
+          </h1>
+          <div className="flex items-center text-gray-400 text-sm border-l-2 border-blue-500 pl-3 py-1">
+            {/* <svg
+              className="w-4 h-4 mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+                clipRule="evenodd"
+              />
+            </svg> */}
+            <span>Updated Aug 25, 2025</span>
+          </div>
+        </div>
 
         <p className="text-gray-300 text-base md:text-lg text-start leading-relaxed">
           securecloudX is built on the strong belief that with the right
