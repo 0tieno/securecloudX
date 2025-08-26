2597e2bb801b96b92d429e148869ff6175d1a8e3
0tieno <ronvi035@gmail.com>
2025-08-26 18:49:20 +0300

    feat: Update Refund Policy content and contact details for clarity and accuracy

--- src/pages/landing/RefundPolicy.jsx
+++ src/pages/landing/RefundPolicy.jsx
@@ -3,12 +3,11 @@ import React from "react";
 export default function RefundPolicy() {
   return (
     <div className="w-full max-w-2xl mx-auto mt-16 mb-16 bg-black text-white px-4">
-      <h1 className="text-3xl font-bold mb-8 text-center">Refund Policy</h1>
+      <h1 className="text-3xl font-bold mb-8 text-start">Refund Policy</h1>
       <section className="mb-6">
         <h2 className="text-xl font-semibold mb-2">Overview</h2>
         <p className="text-gray-200">
-          This Refund Policy applies to all purchases made through the website
-          Time.fyi. By making a purchase, you agree to the terms of this policy.
+          This Refund Policy applies to all purchases made for the consultation services on the website securecloudX. By making a purchase, you agree to the terms of this policy.
           If, for any reason, you are not completely satisfied with your
           purchase, you are entitled to request a refund within 15 days of the
           payment date.
@@ -21,11 +20,6 @@ export default function RefundPolicy() {
             The request for a refund must be made within 15 days of the payment
             date.
           </li>
-          <li>
-            For a recurring subscription, the refund request must be made within
-            15 days of the initial payment date for the first month of the
-            subscription.
-          </li>
         </ul>
       </section>
       <section className="mb-6">
@@ -34,16 +28,16 @@ export default function RefundPolicy() {
           <li>
             <span className="font-semibold">Email Us:</span> Send an email to{" "}
             <a
-              href="mailto:kamranahmed.se@gmail.com"
+              href="mailto:securecloudx.learn@gmail.com"
               className="text-blue-400 underline"
             >
-              kamranahmed.se@gmail.com
+              securecloudx.learn@gmail.com
             </a>{" "}
             with the subject line "Refund Request".
           </li>
           <li>
             <span className="font-semibold">Provide Details:</span> In the
-            email, include your name, date of purchase, and the reason for the
+            email, include your name, phone number used to make payments, date of purchase, and the reason for the
             refund request.
           </li>
           <li>
@@ -55,10 +49,7 @@ export default function RefundPolicy() {
       <section className="mb-6">
         <h2 className="text-xl font-semibold mb-2">Refund Process</h2>
         <p className="text-gray-200">
-          Once your refund request is approved, the refund will be processed,
-          and a credit will automatically be applied to your original method of
-          payment. The time it takes for the refund to reflect in your account
-          may vary depending on your bank or credit card issuer.
+          Once your refund request is approved, the refund will be processed immediately.
         </p>
       </section>
       <section className="mb-6">
@@ -67,10 +58,10 @@ export default function RefundPolicy() {
           If you have any questions or concerns about our refund policy, please
           contact us at{" "}
           <a
-            href="mailto:kamranahmed.se@gmail.com"
+            href="mailto:securecloudx.learn@gmail.com"
             className="text-blue-400 underline"
           >
-            kamranahmed.se@gmail.com
+            securecloudx.learn@gmail.com
           </a>
           .
         </p>
