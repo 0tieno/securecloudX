3bd90e2b8072a3c425f2d10467cfa88cb2090478
0tieno <ronvi035@gmail.com>
2026-04-12 12:02:15 +0300

    feat: add changelog data for site updates and features

---
+++ src/data/changelogData.js
@@ -0,0 +1,36 @@
+export const changelog = [
+    {
+        date: "02 Sept, 2025",
+        desc: "Added open source blogs and labs on pentesting.",
+    },
+    {
+        date: "01 Sept, 2025",
+        desc: "Redesigned the site with security vibe and look.",
+    },
+    {
+        date: "26 Aug, 2025",
+        desc: "Added this landing page.",
+    },
+    {
+        date: "22 Aug, 2025",
+        desc: "Added consultation services due to user demand.",
+    },
+    { date: "12 Aug, 2025", desc: "Added more lab tasks on data security" },
+    {
+        date: "13 July, 2025",
+        desc: "Mini-hackathon on git forensics. 34 participants.",
+    },
+    { date: "04 June, 2025", desc: "Introduced weekly challenge format" },
+    {
+        date: "13 May, 2025",
+        desc: "New features and improvements based on user feedback.",
+    },
+    {
+        date: "10 April, 2025",
+        desc: "Officially launched the site to the public",
+    },
+    {
+        date: "8 April, 2025",
+        desc: "Prelaunced the platform via a community meetup call",
+    },
+];
