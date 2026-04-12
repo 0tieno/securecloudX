c9a5463eb0b209e4d0358418591531184535032f
0tieno <ronvi035@gmail.com>
2026-04-12 08:53:29 +0300

    refactor: modularize routing in App component by utilizing route configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,245 +1,41 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ProtectedDayRoute from "./routes/ProtectedDayRoute";
-import ProtectedDayResources from "./routes/ProtectedDayResources";
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 
-import Sidebar from "./components/Sidebar";
-import OpenSourceBlog from "./pages/OpenSourceBlog";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import LandingPage from "./pages/LandingPage";
-import GetStartedPage from "./pages/GetStartedPage";
-import RefundPolicyPage from "./pages/RefundPolicyPage";
-import Day1 from "./pages/Day1";
-import Task1 from "./pages/Task1";
-import Day2 from "./pages/Day2";
-import Task2 from "./pages/Task2";
-import Day3 from "./pages/Day3";
-import Task3 from "./pages/Task3";
-import Day4 from "./pages/Day4";
-import Task4 from "./pages/Task4";
-import Day5 from "./pages/Day5";
-import Task5 from "./pages/Task5";
-import Day6 from "./pages/Day6";
-import Task6 from "./pages/Task6";
-import Day7 from "./pages/Day7";
-import Task7 from "./pages/Task7";
-import Footer from "./components/Footer";
-import Resources from "./pages/Resources";
-import Explore from "./pages/Explore";
-import BeginnerIntro from "./pages/BeginnerIntro";
-import Phase1 from "./pages/task3/Phase1";
-import Phase2 from "./pages/task3/Phase2";
-import Phase3 from "./pages/task3/Phase3";
-import BlogList from "./pages/BlogList";
-import BlogPost from "./pages/BlogPost";
-import ForgottenSecretLab from "./pages/ForgottenSecretLab";
-import PentestingLabs from "./pages/PentestingLabs";
-import TermsOfService from "./pages/TermsOfService";
-import Changelog from "./pages/Changelog";
-import Pricing from "./pages/Pricing";
-import About from "./pages/AboutAuthor";
-import Story from "./pages/Story";
-import Work from "./pages/Work";
-import SafaricomCTF2025 from "./pages/SafaricomCTF2025";
-import RealIPHeistWriteup from "./pages/RealIPHeistWriteup";
+import AppShell from "./components/AppShell";
+import ProtectedDayRoute from "./routes/ProtectedDayRoute";
+import { shellRoutes, standaloneRoutes } from "./routes/routeConfig";
 
 const App = () => {
+  const renderRouteElement = (route) => {
+    const element = <route.Component />;
+
+    if (!route.protectedDay) {
+      return element;
+    }
+
+    return <ProtectedDayRoute day={route.protectedDay}>{element}</ProtectedDayRoute>;
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/refund-policy" element={<RefundPolicyPage />} />
-        <Route path="/terms-of-service" element={<TermsOfService />} />
-        <Route path="/get-started" element={<GetStartedPage />} />
-        <Route path="/changelog" element={<Changelog />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/story" element={<Story />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/ctf/safaricom-2025" element={<SafaricomCTF2025 />} />
-        <Route
-          path="/ctf/safaricom-2025/real-ip-heist"
-          element={<RealIPHeistWriteup />}
-        />
+        {standaloneRoutes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={<route.Component />}
+          />
+        ))}
 
-        <Route
-          path="*"
-          element={
-            <div className="flex h-screen bg-gray-900">
-              <Sidebar />
-              <div className="flex flex-col flex-1 h-screen overflow-hidden">
-                <Header />
-                <div className="flex-1 overflow-y-auto p-6 bg-gray-900 border-l border-gray-700">
-                  <Routes>
-                    <Route path="/home" element={<Home />} />
-                    <Route
-                      path="/forgotten-secret-lab"
-                      element={<ForgottenSecretLab />}
-                    />
-                    <Route
-                      path="/day1"
-                      element={
-                        <ProtectedDayRoute day={1}>
-                          <Day1 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day1/task"
-                      element={
-                        <ProtectedDayRoute day={1}>
-                          <Task1 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day2"
-                      element={
-                        <ProtectedDayRoute day={2}>
-                          <Day2 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day2/task"
-                      element={
-                        <ProtectedDayRoute day={2}>
-                          <Task2 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day3"
-                      element={
-                        <ProtectedDayRoute day={3}>
-                          <Day3 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day3/task"
-                      element={
-                        <ProtectedDayRoute day={3}>
-                          <Task3 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day3/task/phase1"
-                      element={
-                        <ProtectedDayRoute day={3}>
-                          <Phase1 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day3/task/phase2"
-                      element={
-                        <ProtectedDayRoute day={3}>
-                          <Phase2 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day3/task/phase3"
-                      element={
-                        <ProtectedDayRoute day={3}>
-                          <Phase3 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day4"
-                      element={
-                        <ProtectedDayRoute day={4}>
-                          <Day4 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day4/task"
-                      element={
-                        <ProtectedDayRoute day={4}>
-                          <Task4 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day5"
-                      element={
-                        <ProtectedDayRoute day={5}>
-                          <Day5 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day5/task"
-                      element={
-                        <ProtectedDayRoute day={5}>
-                          <Task5 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day6"
-                      element={
-                        <ProtectedDayRoute day={6}>
-                          <Day6 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day6/task"
-                      element={
-                        <ProtectedDayRoute day={6}>
-                          <Task6 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day7"
-                      element={
-                        <ProtectedDayRoute day={7}>
-                          <Day7 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day7/task"
-                      element={
-                        <ProtectedDayRoute day={7}>
-                          <Task7 />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route
-                      path="/day/:day/resources"
-                      element={<ProtectedDayResources />}
-                    />
-                    <Route
-                      path="/explore"
-                      element={
-                        <ProtectedDayRoute day={7}>
-                          <Explore />
-                        </ProtectedDayRoute>
-                      }
-                    />
-                    <Route path="/start" element={<BeginnerIntro />} />
-                    <Route path="/posts" element={<BlogList />} />
-                    <Route path="/posts/:id" element={<BlogPost />} />
-                  </Routes>
-                </div>
-                <Footer />
-              </div>
-            </div>
-          }
-        />
-
-        {/* Standalone OpenSourceBlog route */}
-        <Route path="/opensource-blog" element={<OpenSourceBlog />} />
-
-        {/* Standalone PentestingLabs route */}
-        <Route path="/pentesting-labs" element={<PentestingLabs />} />
+        <Route element={<AppShell />}>
+          {shellRoutes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={renderRouteElement(route)}
+            />
+          ))}
+          <Route path="*" element={<Navigate to="/home" replace />} />
+        </Route>
       </Routes>
     </Router>
   );
