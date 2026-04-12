6db186244f31d29fb99e94d2dc50127216e54545
0tieno <ronvi035@gmail.com>
2026-04-12 08:53:37 +0300

    feat: add AppShell component for layout structure

---
+++ src/components/AppShell.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+
+import Footer from "./Footer";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+const AppShell = () => {
+  return (
+    <div className="flex h-screen bg-gray-900">
+      <Sidebar />
+      <div className="flex flex-1 flex-col h-screen overflow-hidden">
+        <Header />
+        <div className="flex-1 overflow-y-auto border-l border-gray-700 bg-gray-900 p-6">
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default AppShell;
