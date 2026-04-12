83309536e8368ee741e67cf8fa926ede90ed52ff
0tieno <ronvi035@gmail.com>
2026-04-12 12:26:27 +0300

    feat: modularize codebase by restructuring routes and components

--- src/routes/routeConfig.jsx
+++ src/routes/routeConfig.jsx
@@ -3,13 +3,13 @@ import BeginnerIntro from "../pages/BeginnerIntro";
 import BlogList from "../pages/BlogList";
 import BlogPost from "../pages/BlogPost";
 import Changelog from "../pages/Changelog";
-import Day1 from "../pages/Day1";
-import Day2 from "../pages/Day2";
-import Day3 from "../pages/Day3";
-import Day4 from "../pages/Day4";
-import Day5 from "../pages/Day5";
-import Day6 from "../pages/Day6";
-import Day7 from "../pages/Day7";
+import Module1 from "../pages/modules/module1/Overview";
+import Module2 from "../pages/modules/module2/Overview";
+import Module3 from "../pages/modules/module3/Overview";
+import Module4 from "../pages/modules/module4/Overview";
+import Module5 from "../pages/modules/module5/Overview";
+import Module6 from "../pages/modules/module6/Overview";
+import Module7 from "../pages/modules/module7/Overview";
 import Explore from "../pages/Explore";
 import ForgottenSecretLab from "../pages/ForgottenSecretLab";
 import GetStartedPage from "../pages/GetStartedPage";
@@ -17,25 +17,24 @@ import Home from "../pages/Home";
 import LandingPage from "../pages/LandingPage";
 import OpenSourceBlog from "../pages/OpenSourceBlog";
 import PentestingLabs from "../pages/PentestingLabs";
-import Phase1 from "../pages/task3/Phase1";
-import Phase2 from "../pages/task3/Phase2";
-import Phase3 from "../pages/task3/Phase3";
+import Phase1 from "../pages/modules/module3/Phase1";
+import Phase2 from "../pages/modules/module3/Phase2";
+import Phase3 from "../pages/modules/module3/Phase3";
 import Pricing from "../pages/Pricing";
 import RealIPHeistWriteup from "../pages/RealIPHeistWriteup";
 import RefundPolicyPage from "../pages/RefundPolicyPage";
 import Resources from "../pages/Resources";
 import SafaricomCTF2025 from "../pages/SafaricomCTF2025";
 import Story from "../pages/Story";
-import Task1 from "../pages/Task1";
-import Task2 from "../pages/Task2";
-import Task3 from "../pages/Task3";
-import Task4 from "../pages/Task4";
-import Task5 from "../pages/Task5";
-import Task6 from "../pages/Task6";
-import Task7 from "../pages/Task7";
+import ModuleTask1 from "../pages/modules/module1/Task";
+import ModuleTask2 from "../pages/modules/module2/Task";
+import ModuleTask3 from "../pages/modules/module3/Task";
+import ModuleTask4 from "../pages/modules/module4/Task";
+import ModuleTask5 from "../pages/modules/module5/Task";
+import ModuleTask6 from "../pages/modules/module6/Task";
+import ModuleTask7 from "../pages/modules/module7/Task";
 import TermsOfService from "../pages/TermsOfService";
 import Work from "../pages/Work";
-import ProtectedDayResources from "./ProtectedDayResources";
 
 export const standaloneRoutes = [
   { path: "/", Component: LandingPage },
@@ -59,25 +58,25 @@ export const standaloneRoutes = [
 export const shellRoutes = [
   { path: "/home", Component: Home },
   { path: "/forgotten-secret-lab", Component: ForgottenSecretLab },
-  { path: "/day1", Component: Day1, protectedDay: 1 },
-  { path: "/day1/task", Component: Task1, protectedDay: 1 },
-  { path: "/day2", Component: Day2, protectedDay: 2 },
-  { path: "/day2/task", Component: Task2, protectedDay: 2 },
-  { path: "/day3", Component: Day3, protectedDay: 3 },
-  { path: "/day3/task", Component: Task3, protectedDay: 3 },
-  { path: "/day3/task/phase1", Component: Phase1, protectedDay: 3 },
-  { path: "/day3/task/phase2", Component: Phase2, protectedDay: 3 },
-  { path: "/day3/task/phase3", Component: Phase3, protectedDay: 3 },
-  { path: "/day4", Component: Day4, protectedDay: 4 },
-  { path: "/day4/task", Component: Task4, protectedDay: 4 },
-  { path: "/day5", Component: Day5, protectedDay: 5 },
-  { path: "/day5/task", Component: Task5, protectedDay: 5 },
-  { path: "/day6", Component: Day6, protectedDay: 6 },
-  { path: "/day6/task", Component: Task6, protectedDay: 6 },
-  { path: "/day7", Component: Day7, protectedDay: 7 },
-  { path: "/day7/task", Component: Task7, protectedDay: 7 },
-  { path: "/day/:day/resources", Component: ProtectedDayResources },
-  { path: "/explore", Component: Explore, protectedDay: 7 },
+  { path: "/module1", Component: Module1 },
+  { path: "/module1/task", Component: ModuleTask1 },
+  { path: "/module2", Component: Module2 },
+  { path: "/module2/task", Component: ModuleTask2 },
+  { path: "/module3", Component: Module3 },
+  { path: "/module3/task", Component: ModuleTask3 },
+  { path: "/module3/task/phase1", Component: Phase1 },
+  { path: "/module3/task/phase2", Component: Phase2 },
+  { path: "/module3/task/phase3", Component: Phase3 },
+  { path: "/module4", Component: Module4 },
+  { path: "/module4/task", Component: ModuleTask4 },
+  { path: "/module5", Component: Module5 },
+  { path: "/module5/task", Component: ModuleTask5 },
+  { path: "/module6", Component: Module6 },
+  { path: "/module6/task", Component: ModuleTask6 },
+  { path: "/module7", Component: Module7 },
+  { path: "/module7/task", Component: ModuleTask7 },
+  { path: "/module/:module/resources", Component: Resources },
+  { path: "/explore", Component: Explore },
   { path: "/start", Component: BeginnerIntro },
   { path: "/posts", Component: BlogList },
   { path: "/posts/:id", Component: BlogPost },
