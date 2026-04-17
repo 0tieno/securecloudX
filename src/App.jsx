9db017b34a74163bda6085a3c842fdfb2acfccc9
0tieno <ronvi035@gmail.com>
2026-04-17 11:27:51 +0300

    feat: implement authentication context and protected routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 
 import AppShell from "./components/AppShell";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./contexts/AuthContext";
 import { shellRoutes, standaloneRoutes } from "./routes/routeConfig";
 
 const App = () => {
   return (
+    <AuthProvider>
     <Router>
       <Routes>
         {standaloneRoutes.map((route) => (
@@ -20,13 +23,18 @@ const App = () => {
             <Route
               key={route.path}
               path={route.path}
-              element={<route.Component />}
+              element={
+                <ProtectedRoute>
+                  <route.Component />
+                </ProtectedRoute>
+              }
             />
           ))}
-          <Route path="*" element={<Navigate to="/home" replace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
     </Router>
+    </AuthProvider>
   );
 };
 
